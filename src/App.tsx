8af1593c7178ff5d40b7d2d915708ac8e7a75cb5
Gn0lee <jinho605@gmail.com>
2024-01-31 20:44:40 +0900

    레이아웃 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+/** @jsxImportSource @emotion/react */
+
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { Global, css } from '@emotion/react';
 
 import Router from 'routes/Router';
 
@@ -12,7 +15,27 @@ const queryClient = new QueryClient({
 export default function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
+			<Global styles={[globalSt]} />
 			<Router />
 		</QueryClientProvider>
 	);
 }
+
+const globalSt = css`
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	html,
+	body {
+		width: 100%;
+		height: 100%;
+		font-size: 16px;
+	}
+
+	#root {
+		height: 100%;
+	}
+`;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 
+import Layout from 'common/layout/Layout';
+
 export default function Router() {
 	return (
 		<Routes>
-			<Route path="/">
+			<Route path="/" element={<Layout />}>
 				<Route path="" element={<h1>홈화면</h1>} />
 				<Route path=":id" element={<h1>상세화면</h1>} />
 			</Route>
